3a18f267ce45e61f3bb4c4c20ff95c018cb61c6e
unnikrishnanr097 <84676252+unnikrishnanr097@users.noreply.github.com>
2023-01-06 17:56:36 +0530

    Add files via upload

---
+++ New_Demo/src/test/java/com/example/New_Demo/NewDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.New_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
